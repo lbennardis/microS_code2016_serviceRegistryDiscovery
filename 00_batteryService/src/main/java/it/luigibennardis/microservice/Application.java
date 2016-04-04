100843b46a04a6ef026a5444d099d7a0290faf4b
Luigi Bennardis <l.bennardis@email.it>
2016-04-04 16:34:43 +0200

    PRIMA VERSIONE INTEGRATA CON EUREKA

--- 00_batteryService/src/main/java/it/luigibennardis/microservice/Application.java
+++ 00_batteryService/src/main/java/it/luigibennardis/microservice/Application.java
@@ -19,12 +19,14 @@ import javax.sql.DataSource;
 
 
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -37,9 +39,12 @@ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 //@SpringBootApplication
 //@EnableDiscoveryClient
 
-@ComponentScan
-@Configuration
-@EnableAutoConfiguration
+//@ComponentScan
+//@Configuration
+//@EnableAutoConfiguration
+
+@SpringBootApplication
+@EnableDiscoveryClient //***AGGIUNTO PER EUREKA
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
