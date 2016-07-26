1b2106ea098e437e6a7d9b13cfa0324a67d9b2c0
Luigi Bennardis <l.bennardis@email.it>
2016-07-26 12:25:51 +0200

    first integration to kafka topic

--- 01_bookABattery_SERVICE/src/main/java/it/luigibennardis/microservice/message/broker/PublishToKafka.java
+++ 01_bookABattery_SERVICE/src/main/java/it/luigibennardis/microservice/message/broker/PublishToKafka.java
@@ -2,6 +2,7 @@ package it.luigibennardis.microservice.message.broker;
 
 import it.luigibennardis.microservice.domain.Booking;
 import it.luigibennardis.microservice.repositories.IBookingInfoRepository;
+import it.luigibennardis.microservice.service.GetPendingBookingInfo;
 
 import java.util.Date;
 import java.util.List;
@@ -9,15 +10,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 @EnableBinding(Source.class)
 public class PublishToKafka {
 	
+	
+	@Autowired
+	private ApplicationContext context;
+	
+	/*
 	@Autowired
 	private final IBookingInfoRepository prenotazioniRepository;
 
@@ -25,27 +34,31 @@ public class PublishToKafka {
     PublishToKafka(IBookingInfoRepository prenotazioniRepository) {
         this.prenotazioniRepository = prenotazioniRepository;
     }
-	
+	*/
     
 	
 	
 	@Bean
 	@InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "10000", maxMessagesPerPoll = "1"))
-	public MessageSource<TimeInfo> timerMessageSource() {
-		
+	//public MessageSource<TimeInfo> timerMessageSource() {
+		public MessageSource<Booking> timerMessageSource() {
+				
     	// CALL SERVICE THAT RETURNS A LIST OF BOOKING RECORD IN PENDING STATE 
 		//Booking prenotaBatteria = null;
 	    
 	    //List <Booking> listItems;
 	    //listItems = prenotazioniRepository.findAll();
-    	
-    	
-        
+    	  	
+		GetPendingBookingInfo  service = context.getBean(GetPendingBookingInfo.class);
+		
+		List <Booking> listaItem = service.getPendingBooking();
+		
+		
     	TimeInfo timeInfo = new TimeInfo(new Date().getTime()+"","Label");
+    	    	
     	
-    	
-    	
-    	return () -> MessageBuilder.withPayload(timeInfo).build();
+    	//return () -> MessageBuilder.withPayload(timeInfo).build();
+    	return () -> MessageBuilder.withPayload(listaItem.get(0)).build();
     	}
 	
 	public static class TimeInfo{
