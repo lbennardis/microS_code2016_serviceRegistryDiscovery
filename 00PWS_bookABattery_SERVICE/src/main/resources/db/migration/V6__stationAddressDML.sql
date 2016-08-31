6664959fce5d5c25ed473c3b3022646cfa4c4def
Luigi Bennardis <l.bennardis@email.it>
2016-08-31 15:44:18 +0200

    create

---
+++ 00PWS_bookABattery_SERVICE/src/main/resources/db/migration/V6__stationAddressDML.sql
@@ -0,0 +1,11 @@
+INSERT INTO stationaddress VALUES('Libia','Viale Libia 200','ROMA',41.93236,12.52021);
+INSERT INTO stationaddress VALUES('Nomentana','Via Nomentana 41','ROMA',41.91054,12.50272);
+INSERT INTO stationaddress VALUES('Villini','Via dei Villini 1','ROMA',41.79311,13.21948);
+INSERT INTO stationaddress VALUES('Bologna','Piazza Bologna 5','ROMA',45.44434,9.22379);
+INSERT INTO stationaddress VALUES('Fiori','Via Adriano Fiori 16','ROMA',41.93486,12.55199);
+INSERT INTO stationaddress VALUES('Romania','Piazza Romania 1','ROMA',41.92475,12.49285);
+INSERT INTO stationaddress VALUES('Muse','Piazza delle Muse 2','ROMA',41.93032,12.49025);
+INSERT INTO stationaddress VALUES('Parioli','Viale Parioli 140','ROMA',41.92806,12.48661);
+INSERT INTO stationaddress VALUES('Corso Italia','Corso Italia 100','ROMA',41.91018,12.50010);
+INSERT INTO stationaddress VALUES('Prati Fiscali','Via dei Prati Fiscali 150','ROMA',41.94657,12.52057);
+INSERT INTO stationaddress VALUES('Vimercati','Piazza Vimercati 1','ROMA',41.95777,12.52421);
